0fb206df3b94f4e3bff06d3ef20d31683bf62109
mjhavard <mjhavard@outlook.com>
2017-02-24 11:47:41 +0000

    added oid login functionality
    part V tutortial

--- app/__init__.py
+++ app/__init__.py
@@ -5,4 +5,15 @@ app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
+import os
+from flask_login import LoginManager
+from flask_openid import OpenID
+from config import basedir
+
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+lm.login_view = 'login'
+
+# do this last
 from app import views, models
